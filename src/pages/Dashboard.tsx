b9f9e36bfe6f8e295ee86c1aedcd4cb1fd36b759
engineer_it <yurchenko-in@voda.crimea.ru>
2026-09-04 16:06:36 +0300

    delete src/hooks/useAuth.ts

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
-import { useAuth } from '../hooks/useAuth';
+//import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../contexts/AuthContext';
 
 export function Dashboard() {
   const { getUser, logout } = useAuth();
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,29 @@
 import { Link } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../contexts/AuthContext';
 import corpIcon from '../assets/voda_logo.png';
 
 export function Home() {
-  const { isAuthenticated, getUser } = useAuth();
-  const user = getUser();
+  // В контексте isAuthenticated - булево, user доступен напрямую
+  const { user, isAuthenticated } = useAuth();
 
   return (
     <>
-      {/**top*/}
       <div className='top'>
-      
         <Link to="/" className="top__logo">
-        <img className="top__logo_img"
-          src={corpIcon} 
-          alt="Ноme" />
+          <img className="top__logo_img" src={corpIcon} alt="Home" />
         </Link>
-     
       </div>
 
       <main className='main'>
-        {/* Навигация */}
         <nav className="nav">
-          
           <h1 className='nav__h1'>Диспетчерская служба</h1>
-        
-          {isAuthenticated() ? (
+          
+          {isAuthenticated ? (  // isAuthenticated - булево, не вызываем как функцию
             <>
               <div className='nav__inner'>
-              <h2 className='nav__h2'>Добро пожаловать, {user?.name}!</h2>
-              <Link className='nav__inner_link' to="/tickets">Заявки</Link>
-              <Link className='nav__inner_link' to="/dashboard">Личная информация</Link>
+                <h2 className='nav__h2'>Добро пожаловать, {user?.name}!</h2>
+                <Link className='nav__inner_link' to="/tickets">Заявки</Link>
+                <Link className='nav__inner_link' to="/dashboard">Личная информация</Link>
               </div>
             </>
           ) : (
@@ -41,12 +34,9 @@ export function Home() {
               </div>
             </>
           )}
-
         </nav>
+      </main>
 
-     </main>
-
-      {/* Footer */}
       <footer className='footer'>
         <p className='footer__p'>© {new Date().getFullYear()} Development by Yurchenko Igor.</p>
       </footer>
